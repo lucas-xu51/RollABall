c34be87287eeaff3278648f25cd724ebfab4d110
Lucas Xu <xuhaozhe51@gmail.com>
2025-01-15 23:15:02 -0800

    changed_3

--- Assets/ballController.cs
+++ Assets/ballController.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class ball : MonoBehaviour
 {
+    public Rigidbody sphereRigibody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        Debug.Log("Resultant Vector: " + inputVector);
+
     }
 }
